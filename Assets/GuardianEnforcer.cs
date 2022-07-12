beaabbc65bde0b12ba9737d4d77ba89c3aa1d927
Ben Godlewski <bgodlewski@wisc.edu>
2022-07-12 18:41:48 -0500

    Added VR integration

--- Assets/GuardianEnforcer.cs
+++ Assets/GuardianEnforcer.cs
@@ -26,6 +26,7 @@ public class GuardianEnforcer : MonoBehaviour
             CenterPlayer();
         }
     }
+    
     void CenterPlayer()
     {
         point1.transform.localPosition = (points[0]);
