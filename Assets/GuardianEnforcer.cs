34661392b4a0dcb5bdf9569db24de1dad3c63edb
Ben Godlewski <bgodlewski@wisc.edu>
2022-07-12 11:59:28 -0500

    Resolved terrain dependencies, added script to center Guardian on playspace but still running into issues. Commented out for now.

---
+++ Assets/GuardianEnforcer.cs
@@ -0,0 +1,59 @@
+/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianEnforcer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enforcer.TrackingChanged += UpdateCenter;
+    }
+
+    void UpdateCenter()
+    {
+        if(OVRManager.boundary.GetConfigured())
+        {
+
+            boundary = OVRManager.boundary.GetDimensions(OVRBoundary.BoundaryType.PlayArea);
+            points = OVRManager.boundary.GetGeometry(OVRBoundary.BoundaryType.PlayArea);
+            CenterPlayer();
+        }
+    }
+    void CenterPlayer()
+    {
+        point1.transform.localPosition = (points[0]);
+        point2.transform.localPosition = (points[1]);
+        point3.transform.localPosition = (points[2]);
+        point4.transform.localPosition = (points[3]);
+
+        Vector3 pointA = midPoint(point1.transform.position, point2.transform.position);
+        Vector3 pointB = midPoint(point3.transform.position, point4.transform.position);
+
+        Vector3 between = pointB - pointA;
+
+        float distance = between.magnitude;
+
+        squareMarker.transform.position = pointA + (between / 2.0f);
+        squareMarker.transform.LookAt(pointB);
+
+        worldContainer.transform.position = squareMarker.transform.position;
+        worldContainer.transform.rotation = squareMarker.transform.rotation;
+    }
+
+    Vector3 midPoint(Vector3 a, Vector3 b)
+    {
+    float x = (a.x + b.x) / 2;
+    float y = (a.y + b.y) / 2;
+    float z = (a.z + b.z) / 2;
+
+    return new Vector3(x, y, z);
+    }
+}
+ */
